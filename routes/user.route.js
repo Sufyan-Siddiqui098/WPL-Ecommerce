17734841636cc7e016e535c2afbdd2f9092e73f9
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-11-21 01:47:26 +0500

    admin or user himself can get user info

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginController, signupController, updateUserController } from "../controller/user.controller.js";
+import { getUserByIdController, loginController, signupController, updateUserController } from "../controller/user.controller.js";
 import { validateFields, validateLogin } from "../middleware/validateUserField.js";
 import { isLogin } from "../middleware/authCheck.js";
 
@@ -12,6 +12,7 @@ router.post("/login", validateLogin, loginController);
 // validate required fields (not password) 
 // Email shouldn't be change (TO DO)
 router.put("/update-user", isLogin, validateFields, updateUserController )
+router.get("/user/:userId", isLogin, getUserByIdController )
 
 
 export default router;
